6ddfb33c7520f3329432f349ebce04d28de0d52b
Görkem BÜYÜK <gorkembuyuk259501@gmail.com>
2024-08-14 12:32:38 +0300

    8.soru

---
+++ src/odevler/bolum01/Q08.java
@@ -0,0 +1,19 @@
+package odevler.bolum01;
+
+public class Q08 { public static void main(String[] args) {
+    
+    long secondsperyear = 31_536_500;
+    long birthsperyear= secondsperyear/7;
+    long deathsperyear= secondsperyear/13;
+    long immigrantsperyear= secondsperyear/45;
+    //Bu kadar yapabildim hocam döngü kullanmadan yapamadım :(
+    
+    
+        
+    
+    
+    
+    
+}
+}
+
