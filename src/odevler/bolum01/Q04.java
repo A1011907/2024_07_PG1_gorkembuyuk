3c69029a14eb0c0eed241df1b8456487bc6e7bbc
Görkem BÜYÜK <gorkembuyuk259501@gmail.com>
2024-08-14 10:43:41 +0300

    4.soru

---
+++ src/odevler/bolum01/Q04.java
@@ -0,0 +1,11 @@
+package odevler.bolum01;
+
+public class Q04 {
+    public static void main(String[] args) {
+        double sonuc = (9.5 * 4.5 - (2.5 * 3)) / (45.5 - 3.5);
+      System.out.println("sonucunuz="+sonuc);
+
+
+    }
+}
+
