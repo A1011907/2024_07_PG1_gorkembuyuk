48d21726c0d13e0b41605b494491ab4026f9a681
Görkem BÜYÜK <gorkembuyuk259501@gmail.com>
2024-08-14 09:30:11 +0300

    2.Soru

--- src/odevler/bolum01/Q02.java
+++ src/odevler/bolum01/Q02.java
@@ -1,4 +1,13 @@
 package odevler.bolum01;
 
 public class Q02 {
+    public static void main(String[] args) {
+        System.out.println("1.javaya hoşgeldiniz..");
+        System.out.println("2.javaya hoşgeldiniz..");
+        System.out.println("3.javaya hoşgeldiniz..");
+        System.out.println("4.javaya hoşgeldiniz..");
+        System.out.println("5.javaya hoşgeldiniz..");
+        
+        
+    }
 }
