f103d8e58117d136af3e37135292eca3eddac416
Görkem BÜYÜK <gorkembuyuk259501@gmail.com>
2024-08-14 10:47:56 +0300

    5.soru

---
+++ src/odevler/bolum01/Q05.java
@@ -0,0 +1,14 @@
+package odevler.bolum01;
+
+public class Q05 {
+    public static void main(String[] args) {
+        double cevre;
+        double yarıcap = 5.5;
+        double alan;
+        double pi = 3.14159265359;
+        cevre = 2 * yarıcap * pi;
+        alan = yarıcap * yarıcap * pi;
+        System.out.println("çevresi=" + cevre);
+        System.out.println("alanı=" + alan);
+    }
+}
