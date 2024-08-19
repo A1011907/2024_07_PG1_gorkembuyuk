7d3ed21ce129c422494f76c687039940f7aff374
Görkem BÜYÜK <gorkembuyuk259501@gmail.com>
2024-08-19 12:12:30 +0300

    bölüm 2 3.soru

---
+++ src/odevler/bolum02/Q03.java
@@ -0,0 +1,19 @@
+package odevler.bolum02;
+
+
+import java.util.Scanner;
+
+public class Q03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen feet değerini girin!");
+        double feet =scanner.nextDouble();
+        double metre = feet * 0.3048;
+        System.out.println("Feetin metre olarak karşılığı = "+metre);
+        
+        
+
+
+
+    }
+}
