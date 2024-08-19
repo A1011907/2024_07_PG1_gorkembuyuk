4d5f2e50f9ebc1e156f3488c7b7827af1a2bccfb
Görkem BÜYÜK <gorkembuyuk259501@gmail.com>
2024-08-19 11:58:53 +0300

    bölüm 2 2.soru

---
+++ src/odevler/bolum02/Q02.java
@@ -0,0 +1,22 @@
+package odevler.bolum02;
+
+import java.nio.channels.Pipe;
+import java.util.Scanner;
+
+public class Q02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen yarı çapı giriniz!");
+        double yarıcap =scanner.nextDouble();
+        System.out.println("Lütfen yükseklik degerini giriniz!");
+        double yukseklık =scanner.nextDouble();
+        double pi = 3.14;5
+        double alan = yarıcap*yarıcap*pi;
+        double hacim = alan*yukseklık;
+        System.out.println("ALAN = "+alan);
+        System.out.println("hacim = "+hacim);
+        
+
+
+    }
+}
