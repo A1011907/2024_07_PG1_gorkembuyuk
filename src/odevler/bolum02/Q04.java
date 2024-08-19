db4567636c88ef2a5df4b87ec16df54ae5b053b8
Görkem BÜYÜK <gorkembuyuk259501@gmail.com>
2024-08-19 14:09:00 +0300

    bölüm 2 4.soru

---
+++ src/odevler/bolum02/Q04.java
@@ -0,0 +1,21 @@
+package odevler.bolum02;
+
+
+import javax.crypto.KeyGenerator;
+import java.util.Scanner;
+
+public class Q04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen ağırlığı pound cinsinden giriniz!");
+        double pound =scanner.nextDouble();
+        double kilogram =pound*0.45359237;
+        System.out.println("ağırlığınıız kilo olarak karşılığı = "+kilogram);
+        
+
+
+
+
+
+    }
+}
