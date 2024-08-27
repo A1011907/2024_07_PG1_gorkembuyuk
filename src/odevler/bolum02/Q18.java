f67f7b7912092dc2cb2d95c292903f171a0ebf79
Görkem BÜYÜK <gorkembuyuk259501@gmail.com>
2024-08-27 10:19:01 +0300

    bölüm 2 18.soru

---
+++ src/odevler/bolum02/Q18.java
@@ -0,0 +1,16 @@
+package odevler.bolum02;
+
+public class Q18 {
+    public static void main(String[] args) {
+
+        // Tablo başlıkları
+        System.out.printf("%-10s %-10s %-10s\n", "a", "b", "Pow(a,b)");
+
+        // Tablo satırları
+        System.out.printf("%-10d %-10d %-10d\n", 1, 2, 1);
+        System.out.printf("%-10d %-10d %-10d\n", 2, 3, 8);
+        System.out.printf("%-10d %-10d %-10d\n", 3, 4, 81);
+        System.out.printf("%-10d %-10d %-10d\n", 4, 5, 1024);
+        System.out.printf("%-10d %-10d %-10d\n", 5, 6, 15625);
+    }
+}
